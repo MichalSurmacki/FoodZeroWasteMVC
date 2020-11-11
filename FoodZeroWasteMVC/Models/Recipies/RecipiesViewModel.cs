9bbc0cde19c466811b301749cc82bba3a950e1a8
Michal Surmacki <michal.surmacki@gmail.com>
2020-11-11 15:50:20 +0100

    Modify Index, add recipies viewmodel

---
+++ FoodZeroWasteMVC/Models/Recipies/RecipiesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodZeroWasteMVC.Models.Recipies
+{
+    public class RecipiesViewModel
+    {
+        public List<RecipieDto> Recipies { get; set; }
+    }
+}
